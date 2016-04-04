d60feb81560e1da4bb0d79f5855d737c218e5890
Sadie Forsythe <SadieForsythe@sadies-mbp.home>
2016-04-04 06:55:10 -0400

    Almost got the add function to work. Just need to figure out how to get it to add to the other original list and stay there even after refreshing the page.

--- js/data.js
+++ js/data.js
@@ -1,31 +1,25 @@
 module.exports = [{
   id: 0,
   name: 'In this Moment',
-  type: 'metal',
   album: 'Black Widow',
 }, {
   id: 1,
   name: 'Breaking Benjamin',
-  type: 'rock',
   album: 'Dark Before Dawn',
 }, {
   id: 2,
   name: 'seether',
-  type: 'rock',
   album: 'Isolate and Medicate',
 }, {
   id: 3,
   name: 'Chance The Rapper',
-  type: 'rap',
   album: 'Acid Rap',
 }, {
   id: 4,
   name: 'Childish Gambino',
-  type: 'rap',
-  album: 'something',
+  album: 'Camp',
 }, {
   id: 5,
   name: 'The Pretty Reckless',
-  type: 'metal',
   album: 'Going to Hell',
 }];
---
+++ public/add.js
@@ -0,0 +1,48 @@
+window.addEventListener('load', function ()  {
+
+    var headline = document.getElementById('headline');
+    headline.textContent = 'Add new list items';
+
+            
+    var gtButton= document.getElementById('gotime');
+    console.log(gtButton);
+
+    gtButton.addEventListener('click', function () {
+ 
+        var source1 = document.getElementById('thing'); 
+       
+        var source2 = document.getElementById('cost'); 
+        
+        var li= document.createElement('li');
+        li.textContent = source2.value + ' by ' + source1.value;
+            
+         
+       
+        var parent = document.getElementById('things');
+        parent.appendChild(li);
+        
+        
+if(document.getElementById('thing').value == '')
+{
+    alert('Username can not be empty.');
+    li.texcontent = null;
+}
+                
+if(document.getElementById('cost').value == '')
+{
+    alert('Username can not be empty.');
+    li.texcontent = null;
+}
+        
+    });  
+}); 
+
+
+/*
+
+ name: function (input) {
+        var name = new RegExp('[A-Za-z]+ [A-Za-z]+'); // create a regex
+        return name.test(input); // test the `input` string on the regex
+    
+    },
+*/
