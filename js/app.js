5b5dfca31af58e8db18da690bde78899d02411a2
jbhurley92 <jbhurley92@gmail.com>
2016-04-04 11:54:30 -0400

    working

--- js/app.js
+++ js/app.js
@@ -1,34 +1,56 @@
 window.addEventListener('load', function() {
-  var _ = require('underscore');
+ var _ = require('underscore');
 
-  var data = require('./data');
-  var musicTemplate = _.template(document.getElementById('music-template').textContent);
+ var data = require('./data');
+ var musicTemplate = _.template(document.getElementById('music-template').textContent);
 
-  for (var i = 0; i < data.length; i++) {
-    var artists = musicTemplate({
-      name: data[i].name,
-      type: data[i].type,
-      album: data[i].album,
-    });
-    console.log(artists);
+ for (var i = 0; i < data.length; i++) {
+   var artists = musicTemplate({
+     name: data[i].name,
+     type: data[i].type,
+     album: data[i].album,
+   });
+   console.log(artists);
 
-    var music = document.createElement('div');
-    music.innerHTML = artists;
-    var parent = document.getElementById('results');
-    parent.appendChild(music);
-    console.log(music);
-
-	
+   var element = document.createElement('div');
+   element.classList.add('music');
+   element.setAttribute('id', 'data-' + data[i].id);
+   element.innerHTML = artists;
+   var parent = document.getElementById('results');
+   parent.appendChild(element);
 }
 
-$(function() {
-$('#results div').draggable({
-	appendTo: "body",
-	helper: "clone"
-		// });
-		// $('#adding div').droppable({
-		//   appendTo: "#adding"
+var search = document.getElementById('search-box');
+search.addEventListener('keyup', function () {
+        var word = search.value;
+        var pattern = new RegExp(search.value);
+
+        for (var i = 0; i < data.length; i++) {
+                var element = document.getElementById('data-' + data[i].id);
+                if (pattern.test(data[i].name.toLowerCase())) {
+                        // Show it
+                        element.classList.remove('hidden');
+                } else {
+                        // Hide it
+                        element.classList.add('hidden');
+                }
+        }
 });
+var search2 = document.getElementById('search-box2');
+search2.addEventListener('keyup', function () {
+        var word2 = search2.value;
+        var pattern2 = new RegExp(search2.value);
+
+        for (var i = 0; i < data.length; i++) {
+                var element2 = document.getElementById('data-' + data[i].id);
+                if (pattern2.test(data[i].type.toLowerCase())) {
+                        // Show
+                        element2.classList.remove('hidden');
+                } else {
+                        // Hide
+                        element2.classList.add('hidden');
+                }
+        }
 });
 
 
